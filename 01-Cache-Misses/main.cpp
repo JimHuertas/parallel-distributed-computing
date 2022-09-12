394d0dca9feeb0e5d0245d3952fc4a61656578a9
JimHuertas <live.land18@gmail.com>
2022-09-12 18:08:16 -0500

    matrix generator working

--- 01-Cache-Misses/main.cpp
+++ 01-Cache-Misses/main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-
-    
+    vector<vector<int >> mtr = matrix_generator(4);
+    print_vector(mtr);
 
 
     return 0;
--- 01-Cache-Misses/matrix_generator.cpp
+++ 01-Cache-Misses/matrix_generator.cpp
@@ -3,23 +3,42 @@
 
 #include <iostream>
 #include <vector>
-
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 using namespace std;
+
+vector<int> vector_ram(int n){
+    int random_num;
+    vector<int> column;
+    for (int i = 0; i < n; i++){
+        random_num = rand() % 10000 + 1;
+        column.push_back(random_num);
+    }
+
+    return column;
+}
 
 //return a matrix vector<vector<int>> did it by a nxn (tam) value
 vector<vector<int>> matrix_generator(int n){
+    srand (time(NULL));
+
     vector<vector<int>> matr;
-
-    vector<int> column;
     for (int i = 0; i < n; i++){
-
-        column.push_back(0);
+        vector<int> one_vector = vector_ram(n);
+        matr.push_back(one_vector);
     }
-       
-
-
 
     return matr;
+}
+
+
+void print_vector(vector<vector<int>> mtr){
+    for(vector<int> i : mtr){
+        for(int j : i){
+            cout<<j<< "  ";
+        }
+        cout<<endl;
+    }
 }
 
 #endif
