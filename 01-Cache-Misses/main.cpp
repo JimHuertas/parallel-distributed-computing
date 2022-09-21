7547af571be33bdb979ecbd1fbf2df49cd9e2ba5
JimHuertas <live.land18@gmail.com>
2022-09-21 04:23:05 -0500

    lab 1 done

--- 01-Cache-Misses/main.cpp
+++ 01-Cache-Misses/main.cpp
@@ -5,18 +5,21 @@
 using namespace std;
 
 unsigned t0, t1;
-#define MAX 10000
+#define MAX 1000
 
 
-double blocked_matrix_multiply(int** A, int** B, int** C, int n){
-    int b = 2;
+double blocked_matrix_multiply(int** A, int** B, int** C, int n, int nro_bloques){
+    // b = tamaño de los bloques;
+    // n = tamaño de la matriz
+    // nro_bloques = numero de bloques que se quiere particionar
+    int b = n/nro_bloques; 
     t0=clock();
-    for(int ii=1; ii < b; ii++)
-     for(int jj=1; jj < b; jj++)
-      for(int kk=1; kk < b; kk++)
-       for(int i =(ii-1); i<ii*n; i++)
-        for(int j=(jj-1); j<jj*n; j++)
-         for(int k=(kk-1); k<kk*n; k++)
+    for(int ii=1; ii < nro_bloques; ii+=b)
+     for(int jj=1; jj < nro_bloques; jj+=b)
+      for(int kk=1; kk < nro_bloques; kk+=b)
+       for(int i =(ii-1)*b; i<ii*b; i++)
+        for(int j=(jj-1)*b; j<jj*b; j++)
+         for(int k=(kk-1)*b; k<kk*b; k++)
           C[i][j] += A[i][k] * B[k][j]; 
     t1=clock();
     return (double(t1-t0)/CLOCKS_PER_SEC);
@@ -51,9 +54,6 @@ double matrix_multiply(int** A, int** B, int** C, int n){
 int main(){
     /* Initialize A and x, assign y = 0 */
     srand (time(NULL));
-    
-    
-
     /********************************************************
     *   Implementar y comparar los 2-bucles anidados        *
     *   FOR presentados en el cap. 2 del libro, pag. 22     *
@@ -73,16 +73,18 @@ int main(){
     /*  Initialize B and C nxn matrix  */
 
     /*CREANDO LOS CASOS DE PRUEBA*/
-    // int init = 100;
-    // for (int i = init; i <= MAX; i+=init*2){
-    //     int **A = create_matrix(i);
-    //     int **B = create_matrix(i);
-    //     int **C = create_matrix(i);
-    //     llenar_mtr(A, i);
-    //     llenar_mtr(B, i);
-    //     empty_mtr(C,i);
-    //     cout<<"Tamaño: "<<i<<endl<<"Demora: "<<matrix_multiply(A,B,C,i)<<" s. \n\n";
-    // }
+    int init = 100;
+    for (int i = init; i <= MAX; i+=init){
+        int **A = create_matrix(i);
+        int **B = create_matrix(i);
+        int **C = create_matrix(i);
+        llenar_mtr(A, i);
+        llenar_mtr(B, i);
+        empty_mtr(C,i);
+        cout<<"Tamano: "<<i<<endl;
+        //cout<<"\t- Clasico: "<<matrix_multiply(A,B,C,i)<<" s. \n";
+        cout<<"\t- En Bloques: "<<blocked_matrix_multiply(A,B,C,i,4)<<" s.\n\n";
+    }
 
     
 
@@ -95,16 +97,16 @@ int main(){
     *******************************************************************/
 
 
-    int init = 1000;
-    for (int i = init; i <= MAX; i+=init*2){
-        int **A = create_matrix(i);
-        int **B = create_matrix(i);
-        int **C = create_matrix(i);
-        llenar_mtr(A, i);
-        llenar_mtr(B, i);
-        empty_mtr(C,i);
-        cout<<"Tamaño: "<<i<<endl<<"Demora: "<<blocked_matrix_multiply(A,B,C,i)<<" s.\n\n";
-    }
+    // int init = 100;
+    // for (int i = init; i <= MAX; i+=init){
+    //     int **A = create_matrix(i);
+    //     int **B = create_matrix(i);
+    //     int **C = create_matrix(i);
+    //     llenar_mtr(A, i);
+    //     llenar_mtr(B, i);
+    //     empty_mtr(C,i);
+        
+    // }
 
 
     return 0;
